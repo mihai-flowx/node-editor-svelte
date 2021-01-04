403f5b2bb3aff70d32fc7afdfa4aa20ffed015e6
Mihai Saru <mihai@flowx.ai>
2021-01-04 17:38:04 +0200

    [tech] Try drag-drop lib (not what I want)

--- src/components/Node.svelte
+++ src/components/Node.svelte
@@ -2,12 +2,8 @@
   export let name = ''
   // export let source: number = null
   // export let target: number = null
-
-  function startDrag(event) {
-    console.log(event)
-  }
 </script>
 
-<div class="flex shadow-lg p-4 border-0 border-gray-600 max-w-xs cursor-pointer" on:mousedown={startDrag}>
+<div class="flex shadow-lg p-4 border-0 border-gray-600 max-w-xs cursor-pointer">
   <h3>{name}</h3>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
+  import { dndzone } from 'svelte-dnd-action'
   import Node from '../components/Node.svelte'
+
+  const nodes = [
+    {
+      id: 1,
+      name: 'send_kafka_request',
+    },
+    {
+      id: 2,
+      name: 'receive_kafka_request',
+    },
+  ]
 </script>
 
 <h1 class="text-red-500 text-lg">Process Editor</h1>
-<div class="flex p-8">
-  <Node name="send_kafka_request" />
+<div class="flex p-8" use:dndzone={{ items: nodes, dragDisabled: true }}>
+  {#each nodes as node}
+    <Node name={node.name} />
+  {/each}
 </div>
 
 <style global lang="postcss">
