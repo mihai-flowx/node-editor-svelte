4cbd278cec4987e3aaa2579eeba937aadcf8e124
Mihai Saru <mihai@flowx.ai>
2021-01-05 11:07:17 +0200

    [nodes] View nodes

--- src/components/Node.svelte
+++ src/components/Node.svelte
@@ -1,9 +1,52 @@
 <script lang="ts">
   export let name = ''
-  // export let source: number = null
-  // export let target: number = null
+  export let parent: HTMLElement = null
+
+  export let startPos = { x: 0, y: 0 }
+
+  let currentPos = startPos
+  let nodeElm = null
+
+  let dragPoint = {
+    x: 0,
+    y: 0,
+  }
+
+  function startDrag(event: MouseEvent) {
+    console.log('START DRAG', event)
+    document.addEventListener('mousemove', dragMove)
+
+    dragPoint = {
+      x: event.pageX - nodeElm.getBoundingClientRect().left,
+      y: event.pageY - nodeElm.getBoundingClientRect().top,
+    }
+
+    console.log(dragPoint)
+  }
+
+  function stopDrag(event) {
+    console.log('STOP DRAG', event)
+    document.removeEventListener('mousemove', dragMove)
+  }
+
+  function dragMove(event: MouseEvent) {
+    currentPos.x = event.pageX - parent.getBoundingClientRect().left - dragPoint.x
+    currentPos.y = event.pageY - parent.getBoundingClientRect().top - dragPoint.y
+  }
 </script>
 
-<div class="flex shadow-lg p-4 border-0 border-gray-600 max-w-xs cursor-pointer">
-  <h3>{name}</h3>
+<div
+  bind:this={nodeElm}
+  class="absolute flex flex-col shadow-lg p-4 border-0 border-gray-600 w-48 flex-shrink-0 cursor-pointer rounded-md"
+  on:mousedown={startDrag}
+  on:mouseup={stopDrag}
+  on:dragstart={() => false}
+  style="transform: translate({currentPos.x}px, {currentPos.y}px)">
+  <span class="absolute top-2/4 w-4 h-4 rounded-full border-blue-500 border-4 z-10" style="left: -0.5rem" />
+  <span class="absolute top-2/4 w-4 h-4 rounded-full border-blue-500 border-4 z-10" style="right: -0.5rem" />
+
+  <h3 class="select-none border-b-2 border-gray-500">{name}</h3>
+
+  <p>left: {parent?.getBoundingClientRect().left}</p>
+  <p>top: {parent?.getBoundingClientRect().top}</p>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,24 +1,46 @@
 <script lang="ts">
-  import { dndzone } from 'svelte-dnd-action'
   import Node from '../components/Node.svelte'
+
+  let viewerElm = null
+  let zoomLevel = 1
 
   const nodes = [
     {
       id: 1,
-      name: 'send_kafka_request',
+      name: 'start',
     },
     {
       id: 2,
+      name: 'send_kafka_request',
+    },
+    {
+      id: 3,
       name: 'receive_kafka_request',
     },
+    {
+      id: 4,
+      name: 'show_search_results',
+    },
+    {
+      id: 5,
+      name: 'end',
+    },
   ]
+
+  function zoomViewer(event: WheelEvent) {
+    console.log(event)
+    const newZoom = zoomLevel + 0.02 * event.deltaY
+    zoomLevel = Math.min(1, Math.max(0.2, newZoom))
+  }
 </script>
 
 <h1 class="text-red-500 text-lg">Process Editor</h1>
-<div class="flex p-8" use:dndzone={{ items: nodes, dragDisabled: true }}>
-  {#each nodes as node}
-    <Node name={node.name} />
-  {/each}
+<div bind:this={viewerElm} class="relative flex w-full h-96 border-black border overflow-hidden" on:wheel={zoomViewer}>
+  <div style="transform: scale({zoomLevel})" class="nodes-wrapper border-red-500 border">
+    {#each nodes as node, i}
+      <Node name={node.name} parent={viewerElm} startPos={{ x: 100 + i * 250, y: 100 + 50 * Math.random() }} />
+    {/each}
+  </div>
 </div>
 
 <style global lang="postcss">
@@ -28,4 +50,8 @@
   /* purgecss end ignore */
 
   @tailwind utilities;
+
+  .nodes-wrapper {
+    transform-origin: 0px 0px 0px;
+  }
 </style>
